95dfb65ee1e5f321ab1a089a7938a38f0205e0e5
Guest User <student@epicodus.com>
2015-08-11 10:34:20 -0700

    add method to clear list of places

--- spec/places_youve_been_spec.rb
+++ spec/places_youve_been_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('places_youve_been')
 
 describe(Place) do
+  before() do
+    Place.clear()
+  end
+
   describe('#where') do
     it("gives the place name") do
       test_place = Place.new("Moscow")
@@ -20,6 +24,15 @@ describe(Place) do
       test_place = Place.new("Iceland")
       test_place.save()
       expect(Place.all()).to(eq([test_place]))
+    end
+  end
+
+  describe('.clear') do
+    it('clear the list of places') do
+      test_place = Place.new("Greenland")
+      test_place.save()
+      Place.clear()
+      expect(Place.all()).to(eq([]))
     end
   end
 end
